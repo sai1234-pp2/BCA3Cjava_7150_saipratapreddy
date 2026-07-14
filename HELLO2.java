aa72058a14b63c1a3e542c53b7e1275fe72e9a5d
sai1234-pp2 <vallurusai2007@gmail.com>
2026-07-14 11:48:16 +0530

    Add files via upload

---
+++ HELLO2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class HELLO2{
+	public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("enter your name");
+	String name = input.nextLine();
+
+	System.out.println("your name is"+ name);
+
+}
+}
+
