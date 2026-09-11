5353f6933c27e7be4e1a992705f120deb4bebb2d
sai1234-pp2 <vallurusai2007@gmail.com>
2026-09-11 13:47:30 +0530

    Add files via upload

---
+++ School students/Student.java
@@ -0,0 +1,8 @@
+package school.students;
+public class Student{
+    public void displayinfo(){
+    String s1="Aayushi";
+    String s2="Vedanshi";
+    System.out.println("Class toppers: "+ s1 + " and " + s2);
+   }
+}
